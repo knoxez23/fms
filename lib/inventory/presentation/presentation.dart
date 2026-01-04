b17cf3f03c59ea4854675c0d785ea1220374f843
Dev <dev@local>
2026-01-05 00:53:03 +0300

    chore(presentation): add inventory presentation barrel

---
+++ lib/inventory/presentation/presentation.dart
@@ -0,0 +1,7 @@
+// Barrel for inventory presentation
+export 'add_inventory_screen.dart';
+export 'inventory.dart';
+export 'inventory_history_screen.dart';
+export 'inventory_screen.dart';
+export 'presentation.dart';
+export '*/*.dart';
