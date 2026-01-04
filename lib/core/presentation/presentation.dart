46f37397e28288b01440e99633053db024bd9340
Dev <dev@local>
2026-01-05 00:53:03 +0300

    chore(presentation): add core presentation barrel

---
+++ lib/core/presentation/presentation.dart
@@ -0,0 +1,11 @@
+// Barrel for core presentation
+export 'presentation.dart';
+export 'themes.dart';
+export 'widgets.dart';
+export 'animations/animated_card.dart';
+export 'errors/*.dart';
+export 'themes/app_colors.dart';
+export 'themes/theme.dart';
+export 'widgets/app_drawer.dart';
+export 'widgets/modern_app_bar.dart';
+export 'widgets/reusable_widgets.dart';
